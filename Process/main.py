aaf6be845bd31dbedde60dac7d923a5d5b2d19f9
Angeliano <angeliano.sousa@gmail.com>
2022-11-02 22:30:48 -0300

    feat: Finalizado

--- Process/main.py
+++ Process/main.py
@@ -1,8 +1,8 @@
-from multiprocessing import Array, Process
+from multiprocessing import Array, Process, Semaphore
 from time import time
 import my_process
 
-RUNNER = 1
+RUNNER = 30
 total_time = 0
 v_random_numbers = []
 array_counter = Array('i', 10)
@@ -19,51 +19,65 @@ def start_count(n_process, jobs):
       jobs[job].join()
     end_time = time()
 
-    total_time += end_time - start_time
+    time_of_execution = end_time - start_time
+    total_time += time_of_execution
     v_random_numbers = []
   
-  print('='*30)
+  print('=-='*13)
   my_process.print_array_counter(array_counter)
   my_process.check_total_numbers_array(array_counter)
-  print(f'Time of execution {total_time/RUNNER} seconds')
+  print(f'Time of execution {time_of_execution} seconds')
+  print('=-='*13)
 
-def instance_process(n_process, jobs):
+def instance_process(n_process, jobs, semaphore):
 
   if n_process == 1:
     my_process.populate_vector(v_random_numbers)
 
-    process = Process(name='Counter 0 to 9', target=my_process.counter_numbers_on_array, args=(v_random_numbers, array_counter, 0, 9))
+    process = Process(name='Counter 0 to 9', target=my_process.counter_numbers_on_array, args=(v_random_numbers, array_counter, 0, 9, semaphore))
     jobs.append(process)
 
   elif n_process == 2:
     my_process.populate_vector(v_random_numbers)
 
     for p in range(0, 10, 5):
-      process = Process(name=f'Counter {p} to {p+4}', target=my_process.counter_numbers_on_array, args=(v_random_numbers, array_counter, p, p+4))
+      process = Process(name=f'Counter {p} to {p+4}', target=my_process.counter_numbers_on_array, args=(v_random_numbers, array_counter, p, p+4, semaphore))
       jobs.append(process)
 
   elif n_process == 5:
     my_process.populate_vector(v_random_numbers)
 
     for p in range(0, 10, 2):
-      process = Process(name=f'Counter {p} to {p+1}', target=my_process.counter_numbers_on_array, args=(v_random_numbers, array_counter, p, p+1))
+      process = Process(name=f'Counter {p} to {p+1}', target=my_process.counter_numbers_on_array, args=(v_random_numbers, array_counter, p, p+1, semaphore))
       jobs.append(process)
 
   elif n_process == 10:
     my_process.populate_vector(v_random_numbers)
 
     for p in range(10):
-      process = Process(name=f'Counter {p}', target=my_process.counter_numbers_on_array, args=(v_random_numbers, array_counter, p, p))
+      process = Process(name=f'Counter {p}', target=my_process.counter_numbers_on_array, args=(v_random_numbers, array_counter, p, p, semaphore))
       jobs.append(process)
 
 def main():
-  n_process   = int(input('How many process: '))
-  jobs        = []
-  # trava     = int(input('Usign lock ?\n'))
-  # semaphore = int(input('Using semaphore ?\n'))
+  print('=-='*13)
+  print('# Pick how much process do you want...')
+  print('- 1 to 1 Process')
+  print('- 2 to 2 Process')
+  print('- 5 to 5 Process')
+  print('- 10 to 10 Process')
+  print('=-='*13)
+  n_process = int(input())
+  semaphore = input('Do you want semaphore (y/n) ? ')
+  jobs      = []
+  print('=-='*13)
 
-  instance_process(n_process, jobs)
-  start_count(n_process, jobs)
-  # v_random_numbers = []
+  for run in range(RUNNER):
+    print(f'Count number: {run}')
+
+    instance_process(n_process, jobs, semaphore)
+    start_count(n_process, jobs)
+    jobs = []
+  
+  print(f'Media time of execution: {total_time/RUNNER}')
 
 main()
--- Process/my_process.py
+++ Process/my_process.py
@@ -1,4 +1,4 @@
-from multiprocessing import current_process
+from multiprocessing import current_process, Semaphore
 from random import randint
 # from time import sleep
 
@@ -20,12 +20,19 @@ def print_array_counter(array_counter):
   for i in range(0, 10):
     print(f'{i} <-> {array_counter[i]}')
 
-def counter_numbers_on_array(v_random_numbers, array_counter, start_in, ends_in):
+def counter_numbers_on_array(v_random_numbers, array_counter, start_in, ends_in, semaphore):
 
   print(f'Start {current_process().name}...')
-
-  for p in v_random_numbers:
-    for key in range(start_in, ends_in+1):
-      if v_random_numbers[p] == key:
-          array_counter[key] += 1
+  if semaphore == 'y':
+    semaphore = Semaphore(1) if semaphore == 'y' else Semaphore(0)
+    with semaphore:
+      for p in v_random_numbers:
+        for key in range(start_in, ends_in+1):
+          if v_random_numbers[p] == key:
+              array_counter[key] += 1
+  else:
+    for p in v_random_numbers:
+      for key in range(start_in, ends_in+1):
+        if v_random_numbers[p] == key:
+            array_counter[key] += 1
           
